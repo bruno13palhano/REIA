a968147b18a33b28eaed77593580bf5639882a71
bruno13palhano <bruno13palhano@gmail.com>
2024-06-14 16:47:08 -0300

    Add Bind and Conduit classes

--- model/src/main/java/com/bruno13palhano/model/component/electric/bind/Conduit.kt
+++ model/src/main/java/com/bruno13palhano/model/component/electric/bind/Conduit.kt
@@ -1,16 +1,15 @@
 package com.bruno13palhano.model.component.electric.bind
 
-import com.bruno13palhano.model.component.Color
 import com.bruno13palhano.model.component.Point
 
-data class Wire(
+data class Conduit(
     override val id: Long,
     override val name: String,
     override val startPoint: Point,
     override val endPoint: Point,
     override val diameter: Float,
     override val length: Float,
-    val color: Color
+    val type: String
 ) : Bind(
         id = id,
         name = name,
