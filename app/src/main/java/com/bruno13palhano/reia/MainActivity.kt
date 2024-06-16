bc6421714cbffd7abbb88f0d4ef8d3b9028ddfc8
bruno13palhano <bruno13palhano@gmail.com>
2024-06-15 21:54:35 -0300

    Set navigation and menus

--- app/src/main/java/com/bruno13palhano/reia/MainActivity.kt
+++ app/src/main/java/com/bruno13palhano/reia/MainActivity.kt
@@ -6,13 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.bruno13palhano.reia.ui.navigation.MainNavGraph
+import com.bruno13palhano.reia.ui.screens.components.BottomMenu
+import com.bruno13palhano.reia.ui.screens.components.DrawerMenu
 import com.bruno13palhano.reia.ui.theme.REIATheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -21,32 +32,39 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             REIATheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                var showBottomMenu by rememberSaveable { mutableStateOf(false) }
+                var gesturesEnabled by rememberSaveable { mutableStateOf(false) }
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DrawerMenu(
+                        drawerState = drawerState,
+                        navController = navController,
+                        gesturesEnabled = gesturesEnabled
+                    ) {
+                        val coroutineScope = rememberCoroutineScope()
+
+                        Scaffold(
+                            bottomBar = { BottomMenu(navController = navController) }
+                        ) { innerPadding ->
+                            MainNavGraph(
+                                modifier = Modifier.padding(innerPadding),
+                                navController = navController,
+                                showBottomMenu = { showBottomMenu = it },
+                                gesturesEnabled = { gesturesEnabled = it }
+                            ) {
+                                coroutineScope.launch {
+                                    drawerState.open()
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    REIATheme {
-        Greeting("Android")
     }
 }
