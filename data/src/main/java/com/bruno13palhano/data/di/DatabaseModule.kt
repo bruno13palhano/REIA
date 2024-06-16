be160e1ea8f93240b53e47701b6019726a52ddf7
bruno13palhano <bruno13palhano@gmail.com>
2024-06-16 16:38:37 -0300

    Add hilt module for database dependency injection

---
+++ data/src/main/java/com/bruno13palhano/data/di/DatabaseModule.kt
@@ -0,0 +1,41 @@
+package com.bruno13palhano.data.di
+
+import android.content.Context
+import cache.BindQueries
+import cache.BoxQueries
+import cache.ElectricQueries
+import com.bruno13palhano.cache.AppDatabase
+import com.bruno13palhano.data.database.DatabaseFactory
+import com.bruno13palhano.data.database.DriverFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+internal object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): AppDatabase {
+        return DatabaseFactory(
+            driverFactory = DriverFactory(context = context)
+        ).createDriver()
+    }
+
+    @Provides
+    @Singleton
+    fun provideElectricQueries(database: AppDatabase): ElectricQueries = database.electricQueries
+
+    @Provides
+    @Singleton
+    fun provideBoxQueries(database: AppDatabase): BoxQueries = database.boxQueries
+
+    @Provides
+    @Singleton
+    fun provideBindQueries(database: AppDatabase): BindQueries = database.bindQueries
+}
