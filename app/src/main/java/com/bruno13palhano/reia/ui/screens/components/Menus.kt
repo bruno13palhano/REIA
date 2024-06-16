59cb2e7ac402f3e1cb50a6f5b4f9b6b5bb96a428
bruno13palhano <bruno13palhano@gmail.com>
2024-06-15 21:53:37 -0300

    Add menus

---
+++ app/src/main/java/com/bruno13palhano/reia/ui/screens/components/Menus.kt
@@ -0,0 +1,166 @@
+package com.bruno13palhano.reia.ui.screens.components
+
+import android.content.res.Configuration
+import androidx.annotation.StringRes
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.bruno13palhano.reia.R
+import com.bruno13palhano.reia.ui.navigation.MainRoutes
+import kotlinx.coroutines.launch
+
+@Composable
+fun BottomMenu(navController: NavController) {
+    val items =
+        listOf(
+            Screen.Home
+        )
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    NavigationBar {
+        items.forEach { screen ->
+            val selected = currentDestination?.selectedRoute(screen = screen)
+
+            NavigationBarItem(
+                label = { Text(text = stringResource(id = screen.resourceId)) },
+                selected = selected == true,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                icon = { Icon(imageVector = screen.icon, contentDescription = null) }
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun DrawerMenu(
+    drawerState: DrawerState,
+    navController: NavController,
+    gesturesEnabled: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val items =
+        listOf(
+            Screen.Home
+        )
+
+    val orientation = LocalConfiguration.current.orientation
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        gesturesEnabled = gesturesEnabled,
+        drawerContent = {
+            ModalDrawerSheet(
+                modifier =
+                    if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                        Modifier.fillMaxWidth(.78F)
+                    } else {
+                        Modifier
+                    },
+                drawerShape = RectangleShape
+            ) {
+                LazyColumn {
+                    stickyHeader {
+                        Text(
+                            modifier =
+                                Modifier
+                                    .padding(24.dp)
+                                    .fillMaxWidth(),
+                            text = stringResource(id = R.string.app_name),
+                            textAlign = TextAlign.Start,
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                    }
+                    items(items = items) { screen ->
+                        val selected = currentDestination?.selectedRoute(screen = screen)
+
+                        NavigationDrawerItem(
+                            shape = RoundedCornerShape(0, 50, 50, 0),
+                            icon = { Icon(imageVector = screen.icon, contentDescription = null) },
+                            label = { Text(text = stringResource(id = screen.resourceId)) },
+                            selected = selected == true,
+                            onClick = {
+                                navController.navigate(screen.route) {
+                                    popUpTo(navController.graph.findStartDestination().id) {
+                                        saveState = true
+                                    }
+                                    launchSingleTop = true
+                                    restoreState = true
+                                }
+                                scope.launch {
+                                    drawerState.close()
+                                }
+                            },
+                            modifier =
+                                Modifier
+                                    .padding(top = 4.dp, bottom = 4.dp, end = 8.dp)
+                                    .height(52.dp)
+                        )
+                    }
+                }
+            }
+        },
+        content = content
+    )
+}
+
+fun NavDestination.selectedRoute(screen: Screen<MainRoutes>): Boolean {
+    return this.hierarchy.any {
+        it.route?.split(".")?.lastOrNull() == screen.route.toString()
+    }
+}
+
+sealed class Screen<T : MainRoutes>(
+    val route: T,
+    val icon: ImageVector,
+    @StringRes val resourceId: Int
+) {
+    data object Home : Screen<MainRoutes>(
+        route = MainRoutes.Home,
+        icon = Icons.Filled.Home,
+        resourceId = R.string.app_name
+    )
+}
