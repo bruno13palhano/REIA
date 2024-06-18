49a3ba18df73b0684d34bd398ea12ad55b5fa49e
bruno13palhano <bruno13palhano@gmail.com>
2024-06-18 12:51:55 -0300

    Add enum adapter to database factory

--- data/src/main/java/com/bruno13palhano/data/database/DatabaseFactory.kt
+++ data/src/main/java/com/bruno13palhano/data/database/DatabaseFactory.kt
@@ -1,11 +1,17 @@
 package com.bruno13palhano.data.database
 
+import app.cash.sqldelight.EnumColumnAdapter
+import cache.Electric
 import com.bruno13palhano.cache.AppDatabase
 
 internal class DatabaseFactory(private val driverFactory: DriverFactory) {
     fun createDatabase(): AppDatabase {
         return AppDatabase(
-            driver = driverFactory.createDriver()
+            driver = driverFactory.createDriver(),
+            ElectricAdapter =
+                Electric.Adapter(
+                    phaseAdapter = EnumColumnAdapter()
+                )
         )
     }
 }
