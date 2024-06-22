b2ac29cefbdbe5ed60b8d53e82021b06c90612eb
bruno13palhano <bruno13palhano@gmail.com>
2024-06-21 21:19:57 -0300

    Refactor

--- app/src/main/java/com/bruno13palhano/reia/ui/screens/components/ComponentsDialog.kt
+++ app/src/main/java/com/bruno13palhano/reia/ui/screens/components/ComponentsDialog.kt
@@ -20,7 +20,6 @@ import com.bruno13palhano.reia.R
 import com.bruno13palhano.reia.ui.screens.components.inputs.CommonFloatField
 import com.bruno13palhano.reia.ui.screens.components.inputs.CommonTextField
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ElectricComponentDialog(
     title: String,
@@ -40,87 +39,56 @@ fun ElectricComponentDialog(
     onOkClick: () -> Unit,
     onDismissClick: () -> Unit
 ) {
-    BasicAlertDialog(
-        onDismissRequest = onDismissClick,
-        modifier = Modifier.clip(shape = RoundedCornerShape(5))
+    ComponentDialog(
+        title = title,
+        onOkClick = onOkClick,
+        onDismissClick = onDismissClick
     ) {
-        Surface(
-            color = MaterialTheme.colorScheme.background
-        ) {
-            Column(
-                modifier = Modifier.padding(8.dp),
-                horizontalAlignment = Alignment.End
-            ) {
-                Text(
-                    modifier = Modifier.padding(8.dp),
-                    text = title
-                )
-                CommonTextField(
-                    modifier = Modifier.padding(4.dp),
-                    value = name,
-                    label = stringResource(id = R.string.name_label),
-                    placeholder = stringResource(id = R.string.enter_name_label),
-                    onValueChange = onNameChange
-                )
-                CommonTextField(
-                    modifier = Modifier.padding(4.dp),
-                    value = circuit,
-                    label = stringResource(id = R.string.circuit_label),
-                    placeholder = stringResource(id = R.string.enter_circuit_label),
-                    onValueChange = onCircuitChange
-                )
-                CommonFloatField(
-                    modifier = Modifier.padding(4.dp),
-                    value = tension,
-                    label = stringResource(id = R.string.tension_label),
-                    placeholder = stringResource(id = R.string.enter_tension_label),
-                    onValueChange = onTensionChange
-                )
-                CommonFloatField(
-                    modifier = Modifier.padding(4.dp),
-                    value = current,
-                    label = stringResource(id = R.string.current_label),
-                    placeholder = stringResource(id = R.string.enter_current_label),
-                    onValueChange = onCurrentChange
-                )
-                CommonFloatField(
-                    modifier = Modifier.padding(4.dp),
-                    value = power,
-                    label = stringResource(id = R.string.power_label),
-                    placeholder = stringResource(id = R.string.enter_power_label),
-                    onValueChange = onPowerChange
-                )
-                CommonTextField(
-                    modifier = Modifier.padding(4.dp),
-                    value = phase,
-                    label = stringResource(id = R.string.phase_label),
-                    placeholder = stringResource(id = R.string.enter_phase_label),
-                    onValueChange = onPhaseChange
-                )
-
-                Row(modifier = Modifier.padding(8.dp)) {
-                    Button(
-                        modifier = Modifier.padding(end = 4.dp),
-                        onClick = {
-                            onDismissClick()
-                            onOkClick()
-                        }
-                    ) {
-                        Text(text = stringResource(id = R.string.ok_label))
-                    }
-                    Button(
-                        modifier = Modifier.padding(start = 4.dp),
-                        onClick = onDismissClick
-                    ) {
-                        Text(text = stringResource(id = R.string.cancel_label))
-                    }
-                }
-            }
-        }
+        CommonTextField(
+            modifier = Modifier.padding(4.dp),
+            value = name,
+            label = stringResource(id = R.string.name_label),
+            placeholder = stringResource(id = R.string.enter_name_label),
+            onValueChange = onNameChange
+        )
+        CommonTextField(
+            modifier = Modifier.padding(4.dp),
+            value = circuit,
+            label = stringResource(id = R.string.circuit_label),
+            placeholder = stringResource(id = R.string.enter_circuit_label),
+            onValueChange = onCircuitChange
+        )
+        CommonFloatField(
+            modifier = Modifier.padding(4.dp),
+            value = tension,
+            label = stringResource(id = R.string.tension_label),
+            placeholder = stringResource(id = R.string.enter_tension_label),
+            onValueChange = onTensionChange
+        )
+        CommonFloatField(
+            modifier = Modifier.padding(4.dp),
+            value = current,
+            label = stringResource(id = R.string.current_label),
+            placeholder = stringResource(id = R.string.enter_current_label),
+            onValueChange = onCurrentChange
+        )
+        CommonFloatField(
+            modifier = Modifier.padding(4.dp),
+            value = power,
+            label = stringResource(id = R.string.power_label),
+            placeholder = stringResource(id = R.string.enter_power_label),
+            onValueChange = onPowerChange
+        )
+        CommonTextField(
+            modifier = Modifier.padding(4.dp),
+            value = phase,
+            label = stringResource(id = R.string.phase_label),
+            placeholder = stringResource(id = R.string.enter_phase_label),
+            onValueChange = onPhaseChange
+        )
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BindComponentDialog(
     title: String,
@@ -134,61 +102,32 @@ fun BindComponentDialog(
     onOkClick: () -> Unit,
     onDismissClick: () -> Unit
 ) {
-    BasicAlertDialog(
-        onDismissRequest = onDismissClick,
-        modifier = Modifier.clip(shape = RoundedCornerShape(5))
+    ComponentDialog(
+        title = title,
+        onOkClick = onOkClick,
+        onDismissClick = onDismissClick
     ) {
-        Surface(color = MaterialTheme.colorScheme.background) {
-            Column(
-                modifier = Modifier.padding(8.dp),
-                horizontalAlignment = Alignment.End
-            ) {
-                Text(
-                    modifier = Modifier.padding(8.dp),
-                    text = title
-                )
-                CommonTextField(
-                    value = name,
-                    label = stringResource(id = R.string.name_label),
-                    placeholder = stringResource(id = R.string.enter_name_label),
-                    onValueChange = onNameChange
-                )
-                CommonFloatField(
-                    value = diameter,
-                    label = stringResource(id = R.string.diameter_label),
-                    placeholder = stringResource(id = R.string.enter_diameter_label),
-                    onValueChange = onDiameterChange
-                )
-                CommonFloatField(
-                    value = length,
-                    label = stringResource(id = R.string.length_label),
-                    placeholder = stringResource(id = R.string.enter_length_label),
-                    onValueChange = onLengthChange
-                )
-
-                Row(modifier = Modifier.padding(8.dp)) {
-                    Button(
-                        modifier = Modifier.padding(end = 4.dp),
-                        onClick = {
-                            onDismissClick()
-                            onOkClick()
-                        }
-                    ) {
-                        Text(text = stringResource(id = R.string.ok_label))
-                    }
-                    Button(
-                        modifier = Modifier.padding(start = 4.dp),
-                        onClick = onDismissClick
-                    ) {
-                        Text(text = stringResource(id = R.string.cancel_label))
-                    }
-                }
-            }
-        }
+        CommonTextField(
+            value = name,
+            label = stringResource(id = R.string.name_label),
+            placeholder = stringResource(id = R.string.enter_name_label),
+            onValueChange = onNameChange
+        )
+        CommonFloatField(
+            value = diameter,
+            label = stringResource(id = R.string.diameter_label),
+            placeholder = stringResource(id = R.string.enter_diameter_label),
+            onValueChange = onDiameterChange
+        )
+        CommonFloatField(
+            value = length,
+            label = stringResource(id = R.string.length_label),
+            placeholder = stringResource(id = R.string.enter_length_label),
+            onValueChange = onLengthChange
+        )
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BoxComponentDialog(
     title: String,
@@ -196,12 +135,53 @@ fun BoxComponentDialog(
     height: String,
     width: String,
     depth: String,
+    type: String,
     onNameChange: (name: String) -> Unit,
     onHeightChange: (height: String) -> Unit,
     onWidthChange: (width: String) -> Unit,
     onDepthChange: (depth: String) -> Unit,
     onOkClick: () -> Unit,
     onDismissClick: () -> Unit
+) {
+    ComponentDialog(
+        title = title,
+        onOkClick = onOkClick,
+        onDismissClick = onDismissClick
+    ) {
+        CommonTextField(
+            value = name,
+            label = stringResource(id = R.string.name_label),
+            placeholder = stringResource(id = R.string.enter_name_label),
+            onValueChange = onNameChange
+        )
+        CommonFloatField(
+            value = height,
+            label = stringResource(id = R.string.height_label),
+            placeholder = stringResource(id = R.string.enter_height_label),
+            onValueChange = onHeightChange
+        )
+        CommonFloatField(
+            value = width,
+            label = stringResource(id = R.string.width_label),
+            placeholder = stringResource(id = R.string.enter_width_label),
+            onValueChange = onWidthChange
+        )
+        CommonFloatField(
+            value = depth,
+            label = stringResource(id = R.string.depth_label),
+            placeholder = stringResource(id = R.string.enter_depth_label),
+            onValueChange = onDepthChange
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ComponentDialog(
+    title: String,
+    onOkClick: () -> Unit,
+    onDismissClick: () -> Unit,
+    content: @Composable () -> Unit
 ) {
     BasicAlertDialog(
         onDismissRequest = onDismissClick,
@@ -216,30 +196,9 @@ fun BoxComponentDialog(
                     modifier = Modifier.padding(8.dp),
                     text = title
                 )
-                CommonTextField(
-                    value = name,
-                    label = stringResource(id = R.string.name_label),
-                    placeholder = stringResource(id = R.string.enter_name_label),
-                    onValueChange = onNameChange
-                )
-                CommonFloatField(
-                    value = height,
-                    label = stringResource(id = R.string.height_label),
-                    placeholder = stringResource(id = R.string.enter_height_label),
-                    onValueChange = onHeightChange
-                )
-                CommonFloatField(
-                    value = width,
-                    label = stringResource(id = R.string.width_label),
-                    placeholder = stringResource(id = R.string.enter_width_label),
-                    onValueChange = onWidthChange
-                )
-                CommonFloatField(
-                    value = depth,
-                    label = stringResource(id = R.string.depth_label),
-                    placeholder = stringResource(id = R.string.enter_depth_label),
-                    onValueChange = onDepthChange
-                )
+
+                content()
+
                 Row(modifier = Modifier.padding(8.dp)) {
                     Button(
                         modifier = Modifier.padding(end = 4.dp),
