7eefa5c6a56f7c17b7141e8fb2978aa1f138cce4
bruno13palhano <bruno13palhano@gmail.com>
2024-06-28 14:28:06 -0300

    Add new methods

--- physica_engine/src/main/java/com/bruno13palhano/physica_engine/Particle.kt
+++ physica_engine/src/main/java/com/bruno13palhano/physica_engine/Particle.kt
@@ -33,6 +33,8 @@ class Particle(
      * may be inaccurate in some cases.
      */
     fun integrate(duration: Double) {
+        if (inverseMass <= 0.0) return
+
         if (duration > 0.0) {
             //Update linear position.
             position.addScaledVector(vector = velocity, scale = duration)
@@ -46,6 +48,110 @@ class Particle(
 
             //Impose drag.
             velocity.scalarMultiplication(damping.pow(duration))
+
+            //Clear the forces.
+            clearAccumulator()
         }
+    }
+
+    fun setParticleMass(mass: Double) {
+        if (mass != 0.0)
+            inverseMass = 1.0 / mass
+    }
+
+    fun getParticleMass(): Double {
+        return if (inverseMass == 0.0) {
+            Double.MAX_VALUE
+        } else {
+            1/inverseMass
+        }
+    }
+
+    fun setParticleInverseMass(inverseMass: Double) {
+        this.inverseMass = inverseMass
+    }
+
+    fun getParticleInverseMass(): Double {
+        return inverseMass
+    }
+
+    fun hasFiniteMass(): Boolean {
+        return inverseMass >= 0.0
+    }
+
+    fun setParticleDamping(damping: Double) {
+        this.damping = damping
+    }
+
+    fun getParticleDamping(): Double {
+        return damping
+    }
+
+    fun setParticlePosition(position: Vector)  {
+        this.position = position
+    }
+
+    fun setParticlePosition(x: Double, y: Double, z: Double) {
+        position.x = x
+        position.y = y
+        position.z = z
+    }
+
+    fun getParticlePosition(position: Vector) {
+        position.x = this.position.x
+        position.y = this.position.y
+        position.z = this.position.z
+    }
+
+    fun getParticlePosition(): Vector {
+        return position
+    }
+
+    fun setParticleVelocity(velocity: Vector) {
+        this.velocity = velocity
+    }
+
+    fun setParticleVelocity(x: Double, y: Double, z: Double) {
+        velocity.x = x
+        velocity.y = y
+        velocity.z = z
+    }
+
+    fun getParticleVelocity(velocity: Vector) {
+        velocity.x = this.velocity.x
+        velocity.y = this.velocity.y
+        velocity.z = this.velocity.z
+    }
+
+    fun getParticleVelocity(): Vector {
+        return velocity
+    }
+
+    fun setParticleAcceleration(acceleration: Vector) {
+        this.acceleration = acceleration
+    }
+
+    fun setParticleAcceleration(x: Double, y: Double, z: Double) {
+        acceleration.x = x
+        acceleration.y = y
+        acceleration.z = z
+    }
+
+    fun getParticleAcceleration(acceleration: Vector) {
+        acceleration.x = this.acceleration.x
+        acceleration.y = this.acceleration.y
+        acceleration.z = this.acceleration.z
+    }
+
+    fun getParticleAcceleration(): Vector {
+        return acceleration
+    }
+
+    fun clearAccumulator() {
+        forceAccumulated.clear()
+    }
+
+    fun addForce(force: Vector) {
+        forceAccumulated.add(force)
     }
 }
